f6c0e60a73fc03f07f537dbc4244e0b66c6d9ed3
hamayonhussain <hamayon.hussain@gmail.com>
2019-02-22 16:33:29 -0600

    formatted the code

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
   gameOver = () => {
     if (this.state.score > this.state.highscore) {
-      this.setState({highscore: this.state.score}, function() {
+      this.setState({ highscore: this.state.score }, function () {
         console.log(this.state.highscore);
       });
     }
@@ -23,24 +23,24 @@ class App extends Component {
       card.count = 0;
     });
     alert(`Game Over :( \nscore: ${this.state.score}`);
-    this.setState({score: 0});
+    this.setState({ score: 0 });
     return true;
   }
 
   clickCount = id => {
     this.state.cards.find((o, i) => {
       if (o.id === id) {
-        if(cards[i].count === 0){
+        if (cards[i].count === 0) {
           cards[i].count = cards[i].count + 1;
-          this.setState({score : this.state.score + 1}, function(){
+          this.setState({ score: this.state.score + 1 }, function () {
             console.log(this.state.score);
           });
           this.state.cards.sort(() => Math.random() - 0.5)
-          return true; 
+          return true;
         } else {
           this.gameOver();
         }
-     }
+      }
     });
   }
   // Map over this.state.cards and render a cardCard component for each card object
